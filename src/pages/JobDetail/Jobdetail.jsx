f34377a531b598331c8a1219d791a697b0380d9a
Divyansh <divyanshmalyan@gmail.com>
2026-06-13 04:24:21 +0530

    Deployment error resolved.

--- src/pages/JobDetail/Jobdetail.jsx
+++ src/pages/JobDetail/Jobdetail.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import "./JobDetail.css";
+import "./Jobdetail.css";
 import { applyJob, checkApplied } from "../../api/applicationApi";
 import useUserStore from "../../store/userStore";
 import { useNavigate } from "react-router-dom";
